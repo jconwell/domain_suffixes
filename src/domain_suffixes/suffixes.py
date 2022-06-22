8f759b8da546fffb6ddf38796d8b2f0991738828
John Conwell <john.conwell@gigamon.com>
2022-06-22 10:17:22 -0700

    fixed bug where intermediate suffix nodes (not root and not leaf) didn't have a metadata object. Also cleaned up the insert logic a lot

--- src/domain_suffixes/suffixes.py
+++ src/domain_suffixes/suffixes.py
@@ -15,7 +15,6 @@ __author__ = "John Conwell"
 __copyright__ = "John Conwell"
 __license__ = "MIT"
 
-# from domain_suffixes.trie_structure import _Trie, PUNY_PREFIX
 
 _logger = logging.getLogger(__name__)
 
@@ -46,41 +45,25 @@ def is_puny_code(label):
 class _Node:
     """A node in the trie structure"""
 
-    def __init__(self, label):
+    def __init__(self, label, metadata):
         # the suffix label for this node
         self.label = label
-
-        # whether this label is the end of a suffix
-        self.is_end = False
+        self.metadata = metadata
 
         # a dictionary of child labels
         self.children = {}
-
-        self.is_tld = False
-        self.is_suffix = False
-        self.metadata = None
-
-    def set_metadata(self, metadata):
-        self.metadata = metadata
-        if isinstance(metadata, _TLDInfo):
-            self.is_tld = True
-            self.is_suffix = False
-        elif isinstance(metadata, _SuffixInfo):
-            self.is_tld = False
-            self.is_suffix = True
-            self.is_end = True
 
 
 class _Trie(object):
     """The trie object"""
 
     def __init__(self):
-        self.root = _Node("")
+        self.root = _Node("", None)
 
     def insert(self, suffix, metadata=None, is_public_suffix=None):
         """Insert a suffix into the trie"""
         node = self.root
-        tld_info = None
+        root_metadata = None
 
         # Loop through each label in suffix in reverse order
         labels = suffix.split(".")[::-1]
@@ -88,18 +71,16 @@ class _Trie(object):
             found_node = node.children.get(label)
             if found_node:
                 node = found_node
+                # set the root's metadata obj if not set yet
+                if root_metadata is None:
+                    root_metadata = node.metadata
             else:
-                new_node = _Node(label)
+                if metadata is None:
+                    # if no metadata passed in, this is not a root TLD
+                    metadata = _SuffixInfo(f"{label}.{node.label}", is_public_suffix, root_metadata)
+                new_node = _Node(label, metadata)
                 node.children[label] = new_node
                 node = new_node
-            if tld_info is None:
-                tld_info = node
-        # add node metadata
-        if is_public_suffix is not None:
-            assert tld_info is not None
-            assert metadata is None
-            metadata = _SuffixInfo(suffix, is_public_suffix, tld_info.metadata)
-        node.set_metadata(metadata)
 
     def get_node(self, labels):
         node = self.root
@@ -267,7 +248,6 @@ class Suffixes:
                 tld_node = root_tlds[tld].metadata
                 if tld_node.puny:
                     self._puny_suffixes[tld_node.puny] = tld_node.suffix
-            # self.enrich_gtlds()
             self.enrich_tld_suffixes()
 
             if save_cache and cache_path:
@@ -369,7 +349,7 @@ class Suffixes:
                 suffix = suffix[2:]
             if "." in suffix:
                 # check for puny tld
-                tld = suffix[suffix.rindex(".") + 1:]
+                # tld = suffix[suffix.rindex(".") + 1:]
                 # add suffix to trie tree
                 self._suffix_trie.insert(suffix, is_public_suffix=is_public_suffix)
             else:
@@ -451,8 +431,9 @@ class Suffixes:
 
 
 def run_test():
-    fqdn = "65.22.218.1"
-    result = Suffixes(read_cache=True).parse(fqdn)
+    suffixes = Suffixes(read_cache=False)
+    fqdn = "sts.amazonaws.com"
+    result = suffixes.parse(fqdn)
     rd = result.registrable_domain
     print(rd)
 
