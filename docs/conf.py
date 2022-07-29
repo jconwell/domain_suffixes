3c3fcc2b39d271bdc29f9981ba894753cf13758f
John Conwell <john.conwell@gigamon.com>
2022-07-29 14:34:58 -0700

    fixing bug where whl file had no py files

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../domain_suffixes")
+module_dir = os.path.join(__location__, "../src/domain_suffixes")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
